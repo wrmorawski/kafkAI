b7e6703e4e4024dc728364c9081053c8562210d0
Wiktor Morawski <wrmorawski@gmail.com>
2024-04-23 09:04:38 +0200

    small changes

--- prompts/prompts.py
+++ prompts/prompts.py
@@ -1,8 +1,18 @@
 MOST_BASIC_PROMPT = {
-    'name': 'basic',
-    'prompt': "Generate a missing chapter for Kafka's novel The Trial."
+    'name': 'mbp',
+    'description': 'This is a most basic prompt for the task.',
+    'prompt': [
+      {
+        'role': 'user',
+        'content': "Generate a missing chapter for Kafka's novel The Trial"
+      },
+    ]
 }
 
 SYS_KAFKA = "Write in the style of Franz Kafka"
 
 # Maybe generate what happens in those chapters.    
+
+# next, add sys prompts 
+
+# Write the mean number of characters for each chapter in Kafka's novel The Trial. 
