3e0bf8d71c6592a1c50d2f7a3edf5473bf3af57d
Wiktor Morawski <wrmorawski@gmail.com>
2024-05-06 13:53:41 +0200

    added installing ollama and pulling models

--- tasks.py
+++ tasks.py
@@ -8,10 +8,24 @@ def remove_pycache(ctx):
     os.system("rm -rf */__pycache__")
     os.system("rm -rf __pycache__")
 
+
 @task
 def list_ollama_models(ctx): 
     ctx.run("ollama list")
 
+
 @task 
 def clear_outputs(ctx):
     pass 
+
+
+@task 
+def install_ollama_linux(ctx):
+    ctx.run("curl -fsSL https://ollama.com/install.sh | sh")
+
+
+@task 
+def ollama_pull(ctx, models):
+    # if more than one model then run inv ollama-pull "model1 model2 model3"
+    for model in models.split():
+        ctx.run(f"ollama pull {model}")
