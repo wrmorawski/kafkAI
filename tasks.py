da8fa38b8899fd9eceb78d19278f2de8c868d910
Wiktor Morawski <wrmorawski@gmail.com>
2024-05-20 16:53:07 +0200

    new task

--- tasks.py
+++ tasks.py
@@ -1,6 +1,9 @@
 from invoke import task
 import os 
 from utils.maintenance import clear_test_outputs
+import toml 
+from scripts.generate_ollama import get_responses
+from prompts.prompts import MOST_BASIC_PROMPT
 
 @task
 def remove_pycache(ctx):
@@ -29,3 +32,29 @@ def ollama_pull(ctx, models):
     # if more than one model then run inv ollama-pull "model1 model2 model3"
     for model in models.split():
         ctx.run(f"ollama pull {model}")
+
+
+@task(optional=["file"])
+def ollama_generate(ctx, models=None, file=None):
+    """"
+    Generates text for models
+
+    optional file argument specifies .toml file with parameters
+
+    example use: 
+    a) inv ollama-generate tinyllama
+    b) inv ollama-generate --file config.toml 
+    """
+
+    if file is not None: 
+        #read_toml_file
+        if not os.path.exists(file):
+            print("File does not exist")
+            return
+        else: 
+            #read all settings from toml file 
+            pass
+    else: 
+        models_list = models.split()
+    
+    get_responses(models_list, MOST_BASIC_PROMPT)
