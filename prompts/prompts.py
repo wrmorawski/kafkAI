a1080c5127ecdc286e06bb314619bb5d46a6a0fe
Wiktor Morawski <wrmorawski@gmail.com>
2024-05-21 22:06:37 +0200

    prompt class and getting prompt in invoke

--- prompts/prompts.py
+++ prompts/prompts.py
@@ -1,15 +1,32 @@
-MOST_BASIC_PROMPT = {
-    'name': 'mbp',
-    'description': 'This is a most basic prompt for the task.',
-    'prompt': [
+class Prompt: 
+    
+    prompts = {}
+
+    def __init__(self, name: str, description: str, prompt: str) -> None:
+        self.name = name
+        self.description = description
+        self.prompt = prompt
+        Prompt.prompts[self.name] = self
+
+    def __str__(self) -> str:
+        return f"Name: {self.name}\nDescription: {self.description}"
+    
+    @classmethod
+    def get_prompt(cls, name):
+        return cls.prompts.get(name)
+
+MOST_BASIC_PROMPT = Prompt(
+    name = 'mbp',
+    description = 'This is a most basic prompt for the task.',
+    prompt= [
       {
         'role': 'user',
         'content': "Generate a missing chapter for Kafka's novel The Trial"
       },
     ]
-}
+)
 
-SYS_KAFKA = "Write in the style of Franz Kafka"
+# SYS_KAFKA = "Write in the style of Franz Kafka"
 
 # Maybe generate what happens in those chapters.    
 
--- tasks.py
+++ tasks.py
@@ -34,18 +34,17 @@ def ollama_pull(ctx, models):
         ctx.run(f"ollama pull {model}")
 
 
-@task(optional=["file"])
-def ollama_generate(ctx, models=None, file=None):
-    """"
+@task(optional=["file", "models", "prompts"],)
+def ollama_generate(ctx, models=None, prompts=None, file=None):
+    """"    
     Generates text for models
 
     optional file argument specifies .toml file with parameters
 
     example use: 
-    a) inv ollama-generate tinyllama
+    a) inv ollama-generate -m tinyllama -p mbp 
     b) inv ollama-generate --file config.toml 
     """
-
     if file is not None: 
         #read_toml_file
         if not os.path.exists(file):
@@ -56,5 +55,6 @@ def ollama_generate(ctx, models=None, file=None):
             pass
     else: 
         models_list = models.split()
-    
-    get_responses(models_list, MOST_BASIC_PROMPT)
+        prompts_list = prompts.split()
+
+    get_responses(models_list, prompts_list)
